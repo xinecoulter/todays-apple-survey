b5a69a1465e18d0c004036b419884431ca2e093b
Christine Coulter <christinetcoulter@gmail.com>
2013-10-28 12:25:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodaysAppleSurvey::Application.config.secret_token = '88e415e7f3cab365896df55eb7b356359f64b5a92c2e0b1e83c103e12fa15ac4b2cd4860e6f9d2824481f09e14a61be4db5f2fb9fff7dcbca05eadd3fd18d6dd'
