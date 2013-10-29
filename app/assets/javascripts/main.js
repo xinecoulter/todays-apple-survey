3a1a703f7de63a68d88eebcab3e227f844e00eaf
Christine Coulter <christinetcoulter@gmail.com>
2013-10-29 11:49:38 -0400

    Comment out back button in welcome index view. Comment out back button event listener in javascript file. Add functionality to retrieve values of answers and sum the score.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,50 +1,54 @@
-var i = 0;
+var i = 0,
+    answers = [],
+    score = 0;
 
 var survey = {
     toggleQuestion: function(index) {
         $(".question:eq("+index+")").toggleClass("visible-question");
     },
-    getWeightValue: function() {
+    getAnswerValue: function() {
+        var weight = parseInt($("input[type=radio]:checked").val());
+        answers.push(weight);
     },
-    disableBackButton: function() {
-        // if (i === 0) {
+    getScore: function() {
+        var j = 0,
+            numberOfAnswers = answers.length;
 
-        // }
+        for (j; j < numberOfAnswers; j++) {
+            score += answers[j];
+        }
+        return score;
     }
 };
 
 $(window).ready(function() {
     $(".question").addClass("hidden-question");
     survey.toggleQuestion(i);
+    console.log("question: " + i);
 
     //event listener for clicking next
     $("#next-button").click(function() {
         if ($("input[type=radio]:checked").size() > 0) {
             survey.toggleQuestion(i);
             i++;
+            survey.getAnswerValue();
             survey.toggleQuestion(i);
             $("input[type=radio]").prop('checked', false);
+            console.log("question: " + i);
+            if (i === $(".question").length) {
+                survey.getScore();
+                console.log(score);
+            }
         }
     });
 
     //event listener for clicking back
-    $("#back-button").click(function() {
-        if (i > 0) {
-            survey.toggleQuestion(i);
-            i--;
-            survey.toggleQuestion(i);
-        }
-    });
+    //temporarily remove back button; not fully built out with functionality for removing answers
+    // $("#back-button").click(function() {
+    //     if (i > 0) {
+    //         survey.toggleQuestion(i);
+    //         i--;
+    //         survey.toggleQuestion(i);
+    //     }
+    // });
 });
-
-// $("input[type=radio]:checked")
-
-// $("input[type=radio]").prop('checked', false);
-
-// $('#view_selection_donut a').click(function() {
-//         var view_type = $(this).attr('id');
-//         $('#view_selection_donut a').removeClass('active');
-//         $(this).toggleClass('active');
-//         toggle_view(view_type);
-//         return false;
-//     });
