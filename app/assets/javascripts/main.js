7300fe23f695797f99e0a0f76453a23dc290ce8b
Christine Coulter <christinetcoulter@gmail.com>
2013-10-29 10:21:56 -0400

    Add event handling for clicking on the next button to hide the last question and show the new question

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,19 +1,32 @@
 var i = 0;
 
 var survey = {
-  showQuestion: function(index) {
-    $(".question:eq("+index+")").toggleClass("visible-question");
-  },
-  getWeightValue: function() {
-  }
+    toggleQuestion: function(index) {
+        $(".question:eq("+index+")").toggleClass("visible-question");
+    },
+    getWeightValue: function() {
+    }
 };
 
 $(window).ready(function() {
-  $(".question").addClass("hidden-question");
-  survey.showQuestion(i);
+    $(".question").addClass("hidden-question");
+    survey.toggleQuestion(i);
+
+    //event listener for clicking next
+    $("#next-button").click(function() {
+        if ($("input[type=radio]:checked").size() > 0) {
+            survey.toggleQuestion(i);
+            i++;
+            survey.toggleQuestion(i);
+            $("input[type=radio]").prop('checked', false);
+        }
+    });
+
 });
 
 // $("input[type=radio]:checked")
+
+// $("input[type=radio]").prop('checked', false);
 
 // $('#view_selection_donut a').click(function() {
 //         var view_type = $(this).attr('id');
