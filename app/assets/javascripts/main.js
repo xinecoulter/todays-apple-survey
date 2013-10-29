29a8b8043f7ff3843309bc1596fee2e4cdcc39b6
Christine Coulter <christinetcoulter@gmail.com>
2013-10-29 10:04:53 -0400

    Add styling to hide questions. Add initial structure to welcome index view. Add initial javascript.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,24 @@
-$(window).ready(function() {
+var i = 0;
 
+var survey = {
+  showQuestion: function(index) {
+    $(".question:eq("+index+")").toggleClass("visible-question");
+  },
+  getWeightValue: function() {
+  }
+};
+
+$(window).ready(function() {
+  $(".question").addClass("hidden-question");
+  survey.showQuestion(i);
 });
+
+// $("input[type=radio]:checked")
+
+// $('#view_selection_donut a').click(function() {
+//         var view_type = $(this).attr('id');
+//         $('#view_selection_donut a').removeClass('active');
+//         $(this).toggleClass('active');
+//         toggle_view(view_type);
+//         return false;
+//     });
