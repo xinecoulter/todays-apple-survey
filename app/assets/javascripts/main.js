0876073ce1833d8852a9b5c3eab8fd03d1861c42
Christine Coulter <christinetcoulter@gmail.com>
2013-10-29 14:29:51 -0400

    Add hash table in javascript to determine parenting style code from score

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,15 +2,37 @@ var i = 0,
     answers = [],
     score = 0;
 
+//16 different parenting style outcomes based on score
+var results = {};
+results["1111"] = "ESTJ";
+results["2111"] = "ESTP";
+results["1211"] = "ESFJ";
+results["2211"] = "ESFP";
+results["1121"] = "ENTJ";
+results["2121"] = "ENTP";
+results["1221"] = "ENFJ";
+results["2221"] = "ENFP";
+results["1112"] = "ISTJ";
+results["2112"] = "ISTP";
+results["1212"] = "ISFJ";
+results["2212"] = "ISFP";
+results["1122"] = "INTJ";
+results["2122"] = "INTP";
+results["1222"] = "INFJ";
+results["2222"] = "INFP";
+
 var survey = {
     toggleQuestion: function(index) {
+        //hides question by index of questions array
         $(".question:eq("+index+")").toggleClass("visible-question");
     },
     getAnswerValue: function() {
+        //retrieves value associated with radio button
         var weight = parseInt($("input[type=radio]:checked").val());
         answers.push(weight);
     },
     getScore: function() {
+        //calculates sum of values associated with all answers
         var j = 0,
             numberOfAnswers = answers.length;
 
@@ -18,13 +40,17 @@ var survey = {
             score += answers[j];
         }
         return score;
+    },
+    getResults: function(score) {
+        //determines results based on score and redirects to todaysapple subscription page
+        console.log("Your parenting style is: " + results[String(score)]);
+        window.location = "http://todaysapple.com/todays-apple-survey/?parentingstyle=" + results[String(score)];
     }
 };
 
 $(window).ready(function() {
     $(".question").addClass("hidden-question");
     survey.toggleQuestion(i);
-    console.log("question: " + i);
 
     //event listener for clicking next
     $("#next-button").click(function() {
@@ -34,10 +60,12 @@ $(window).ready(function() {
             survey.getAnswerValue();
             survey.toggleQuestion(i);
             $("input[type=radio]").prop('checked', false);
-            console.log("question: " + i);
             if (i === $(".question").length) {
+                $("#next-button").remove();
                 survey.getScore();
+                console.log(answers);
                 console.log(score);
+                survey.getResults(score);
             }
         }
     });
