2e529cd52e2d76c36135617afb0efebe285b2d4c
Christine Coulter <christinetcoulter@gmail.com>
2013-11-05 15:05:56 -0500

    Change indenting in JS file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 var i = 0,
-    answers = [],
-    score = 0;
+  answers = [],
+  score = 0;
 
 //16 different parenting style outcomes based on score
 var results = {};
@@ -22,47 +22,47 @@ results["1222"] = "INFJ";
 results["2222"] = "INFP";
 
 var survey = {
-    toggleQuestion: function(index) {
-        //shows/hides question by index of questions array
-        // $(".question:eq("+index+")").toggleClass("visible-question");
-        // $(".question:eq("+index+")").delay(1500).fadeToggle();
-        setTimeout(function () {
-            $("#question-number").text("Question: " + String(i+1) + "/" + String($(".question").length));
-            $(".question:eq("+index+")").toggleClass("visible-question");
-        }, 1000);
-    },
-    getAnswerValue: function() {
-        //retrieves value associated with radio button
-        var weight = parseInt($("input[type=radio]:checked").val());
-        answers.push(weight);
-    },
-    getScore: function() {
-        //calculates sum of values associated with all answers
-        var j = 0,
-            numberOfAnswers = answers.length;
+  toggleQuestion: function (index) {
+    //shows/hides question by index of questions array
+    // $(".question:eq("+index+")").toggleClass("visible-question");
+    // $(".question:eq("+index+")").delay(1500).fadeToggle();
+    setTimeout(function () {
+      $("#question-number").text("Question: " + String(i + 1) + "/" + String($(".question").length));
+      $(".question:eq(" + index + ")").toggleClass("visible-question");
+    }, 1000);
+  },
+  getAnswerValue: function () {
+    //retrieves value associated with radio button
+    var weight = parseInt($("input[type=radio]:checked").val());
+    answers.push(weight);
+  },
+  getScore: function () {
+    //calculates sum of values associated with all answers
+    var j = 0,
+        numberOfAnswers = answers.length;
 
-        for (j; j < numberOfAnswers; j++) {
-            score += answers[j];
-        }
-        return score;
-    },
-    getResults: function(score) {
-        //determines results based on score and calls redirectToSubscription after 1.5seconds (enough time to finish progress animation)
-        console.log("Your parenting style is: " + results[String(score)]);
-        $("#progress").removeClass();
-        setTimeout(function() {
-            survey.redirectToSubscription();
-        }, 1500);
-    },
-    redirectToSubscription: function() {
-        //redirects to todaysapple subscription page
-        window.location = "http://todaysapple.com/parenting-style/?parentingstyle=" + results[String(score)];
-    },
-    increaseProgressBar: function(index) {
-        var increment = (100 / $(".question").length);
-        $(".bar").animate({
-            width: String(increment * index) + "%"
-        }, 400, "linear", survey.toggleQuestion(index));
+    for (j; j < numberOfAnswers; j++) {
+      score += answers[j];
+    }
+    return score;
+  },
+  getResults: function (score) {
+    //determines results based on score and calls redirectToSubscription after 1.5seconds (enough time to finish progress animation)
+    console.log("Your parenting style is: " + results[String(score)]);
+    $("#progress").removeClass();
+    setTimeout(function () {
+      survey.redirectToSubscription();
+    }, 1500);
+  },
+  redirectToSubscription: function () {
+    //redirects to todaysapple subscription page
+    window.location = "http://todaysapple.com/parenting-style/?parentingstyle=" + results[String(score)];
+  },
+  increaseProgressBar: function (index) {
+    var increment = (100 / $(".question").length);
+    $(".bar").animate({
+      width: String(increment * index) + "%"
+    }, 400, "linear", survey.toggleQuestion(index));
 
         // $(".bar").animate({
         //     width: String(increment * index) + "%"
@@ -75,7 +75,7 @@ var survey = {
         //         $(".question:eq("+index+")").fadeToggle();
         //     }
         // });
-    }
+  }
 };
 
 // $('html, body').animate({
@@ -88,72 +88,72 @@ var survey = {
 //         alert('test');
 //     }
 
-$(window).ready(function() {
-    $(".question").addClass("hidden-question");
-    survey.toggleQuestion(i);
+$(window).ready(function () {
+  $(".question").addClass("hidden-question");
+  survey.toggleQuestion(i);
 
-    //event listener for clicking the start button
-    $("#start-button").click(function() {
-        $("#start-button").addClass("hidden");
-        $("#survey-description").addClass("hidden");
-        $("#questions-panel").slideDown("slow");
-        $(".barBg").css("visibility", "visible");
+  //event listener for clicking the start button
+  $("#start-button").click(function () {
+    $("#start-button").addClass("hidden");
+    $("#survey-description").addClass("hidden");
+    $("#questions-panel").slideDown("slow");
+    $(".barBg").css("visibility", "visible");
+  });
+
+  //event listener for clicking to learn more detailed description popup windows. semi-functioning
+  $("#floatbar").click(function (e) {
+    e.preventDefault();
+    $(this).find(".popup").fadeIn("slow");
+  });
+
+  //event listener for selecting radio button text
+  //sets all radiowrapper divs to light gray and makes radiowrapper of selected input mint green
+  $("input[type=radio]").click(function (e) {
+    $("input[type=radio]").parent().css("background-color", "rgb(245, 245, 245)");
+    $("input[type=radio]:checked").parent().css("background-color", "rgb(182, 238, 182)");
+
+
+    //event listeners for hovering over radio button text
+    //sets radiowrapper to light green when the mouse enters, if it is not the parradiowrapper div of the checked input
+    $(".radiowrapper").mouseenter(function () {
+      if ($(this).css("background-color") !== "rgb(182, 238, 182)") {
+        $(this).css("background-color", "rgb(231, 255, 231)");
+      }
     });
-
-    //event listener for clicking to learn more detailed description popup windows. semi-functioning
-    $("#floatbar").click(function(e) {
-        e.preventDefault();
-        $(this).find(".popup").fadeIn("slow");
+    //sets radiowrapper to light gray when the mouse leaves, if it is not the parradiowrapper div of the checked input
+    $(".radiowrapper").mouseleave(function () {
+      if ($(this).css("background-color") !== "rgb(182, 238, 182)") {
+        $(this).css("background-color", "rgb(245, 245, 245)");
+      }
     });
+  });
 
-    //event listener for selecting radio button text
-    //sets all radiowrapper divs to light gray and makes radiowrapper of selected input mint green
-    $("input[type=radio]").click(function(e) {
-        $("input[type=radio]").parent().css("background-color", "rgb(245, 245, 245)");
-        $("input[type=radio]:checked").parent().css("background-color", "rgb(182, 238, 182)");
+  //event listener for clicking on description buttons
+  $(".description-button").click(function () {
+    $(".description-button").toggleClass("hidden");
+    $(".description-div").toggleClass("hidden");
+  });
 
+  //event listener for clicking next
+  $("#next-button").click(function () {
+    if ($("input[type=radio]:checked").size() > 0) {
+      $(".description-button").toggleClass("hidden");
+      $(".description-div").toggleClass("hidden");
 
-        //event listeners for hovering over radio button text
-        //sets radiowrapper to light green when the mouse enters, if it is not the parent radiowrapper div of the checked input
-        $(".radiowrapper").mouseenter(function() {
-            if ($(this).css("background-color") !== "rgb(182, 238, 182)") {
-                $(this).css("background-color", "rgb(231, 255, 231)");
-            }
-        });
-        //sets radiowrapper to light gray when the mouse leaves, if it is not the parent radiowrapper div of the checked input
-        $(".radiowrapper").mouseleave(function() {
-            if ($(this).css("background-color") !== "rgb(182, 238, 182)") {
-                $(this).css("background-color", "rgb(245, 245, 245)");
-            }
-        });
-    });
-
-    //event listener for clicking on description buttons
-    $(".description-button").click(function() {
-        $(".description-button").toggleClass("hidden");
-        $(".description-div").toggleClass("hidden");
-    });
-
-    //event listener for clicking next
-    $("#next-button").click(function() {
-        if ($("input[type=radio]:checked").size() > 0) {
-            $(".description-button").toggleClass("hidden");
-            $(".description-div").toggleClass("hidden");
-
-            survey.toggleQuestion(i);
-            i++;
-            survey.getAnswerValue();
-            survey.increaseProgressBar(i);
-            $("input[type=radio]").prop('checked', false);
-            if (i === $(".question").length) {
-                $("#next-button").remove();
-                $("#question-number").addClass("hidden");
-                $(".question").remove(); //new
-                survey.getScore();
-                console.log(answers);
-                console.log(score);
-                survey.getResults(score);
-            }
-        }
-    });
+      survey.toggleQuestion(i);
+      i++;
+      survey.getAnswerValue();
+      survey.increaseProgressBar(i);
+      $("input[type=radio]").prop('checked', false);
+      if (i === $(".question").length) {
+        $("#next-button").remove();
+        $("#question-number").addClass("hidden");
+        $(".question").remove(); //new
+        survey.getScore();
+        console.log(answers);
+        console.log(score);
+        survey.getResults(score);
+      }
+    }
+  });
 });
